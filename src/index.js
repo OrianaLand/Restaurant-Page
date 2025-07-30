4c828020f7722facafce3cb2bab80e79190b9413
Oriana <98231498+OrianaLand@users.noreply.github.com>
2025-07-30 20:10:32 -0300

    Switch from dynamic to static navbar icon handling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import "./styles.css";
 import { pageLoad } from "./home";
+import { menuLoad } from "./menu";
 
 const homeBtn = document.querySelector(".home");
 const menuBtn = document.querySelector(".menu");
 const aboutBtn = document.querySelector(".about");
 
-homeBtn.addEventListener("click", () => alert("home clicked"));
-menuBtn.addEventListener("click", () => alert("menu clicked"));
+homeBtn.addEventListener("click", () => pageLoad());
+menuBtn.addEventListener("click", () => menuLoad());
 aboutBtn.addEventListener("click", () => alert("about clicked"));
 console.log("Hello, are you hungry?");
 
