ee486980d9c932e7882430cb5324e3d453d5728f
Oriana <98231498+OrianaLand@users.noreply.github.com>
2025-07-31 17:50:45 -0300

    Style Home tab components and layout

--- src/home.js
+++ src/home.js
@@ -1,12 +1,24 @@
+import icon from "./images/cooking-icon.png";
 export const pageLoad = () => {
   const content = document.querySelector("#content");
   content.innerHTML = "";
+
+  const phoneIcon = document.createElement("img");
+  phoneIcon.src = icon;
+  phoneIcon.classList.add("small-screen-icon");
+
+  const homeContent = document.createElement("div");
+  homeContent.classList.add("home-content");
+
   const headline = document.createElement("h2");
+  headline.textContent = "Welcome to Hua Mulan's Wok";
+
   const homeMessage = document.createElement("p");
-  headline.textContent = "Welcome to Soul of flavors";
   homeMessage.textContent =
     "Step into a world where every bite tells a story. At Flavors of Joy, we blend tradition with creativity to serve dishes that delight the senses. Whether you're craving a comforting classic or an exciting new flavor, our kitchen crafts each plate with care, passion, and the freshest ingredients. Come hungry — leave inspired.";
-  content.append(headline, homeMessage);
+
+  homeContent.append(headline, homeMessage, phoneIcon);
+  content.append(homeContent);
 };
 
 pageLoad();
